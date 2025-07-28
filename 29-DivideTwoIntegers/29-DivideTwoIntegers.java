3d68926d4cf5ea54b83ca55d760af99b5d246a1d
AnjaniChilumula123 <93757236+AnjaniChilumula123@users.noreply.github.com>
2025-07-28 12:33:00 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -0,0 +1,7 @@
+// Last updated: 7/28/2025, 12:33:01 PM
+class Solution {
+    public int divide(int dividend, int divisor) {
+             double result = (double) dividend /divisor;
+        return (int) result;
+    }
+}
