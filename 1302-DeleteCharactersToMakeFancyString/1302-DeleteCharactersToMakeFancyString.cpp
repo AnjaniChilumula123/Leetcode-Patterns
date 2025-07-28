d06b9eb71ce5aa4b4a3e163a7eb2f7df47890fc0
AnjaniChilumula123 <93757236+AnjaniChilumula123@users.noreply.github.com>
2025-07-28 12:29:14 +0530

    Create 1302-DeleteCharactersToMakeFancyString

---
+++ 1302-DeleteCharactersToMakeFancyString/1302-DeleteCharactersToMakeFancyString.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/28/2025, 12:29:16 PM
+class Solution {
+public:
+    static string makeFancyString(string& s) {
+        int n=s.size(), i=0;
+        char prev='@';
+        for(int len=0; char c: s){
+            if (prev!=c) len=1;
+            else len++;
+            if (len<= 2) 
+                s[i++]=c;
+            prev=c;
+            
+        }
+        s.resize(i);
+        return s;
+         
+    }
+};
