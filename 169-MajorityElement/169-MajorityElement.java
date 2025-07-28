5aebbe4684b6f59cf4761a97264a7580183d90e2
AnjaniChilumula123 <93757236+AnjaniChilumula123@users.noreply.github.com>
2025-07-28 12:31:17 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,19 @@
+// Last updated: 7/28/2025, 12:31:18 PM
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i]))
+                 map.put(nums[i],map.get(nums[i])+1);
+            else
+                map.put(nums[i],0);
+            
+        }
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i])>=nums.length/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
