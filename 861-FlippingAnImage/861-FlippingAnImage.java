7c3605ee8758cc86b5b2c9f587e09a77218fc120
AnjaniChilumula123 <93757236+AnjaniChilumula123@users.noreply.github.com>
2025-07-28 12:29:44 +0530

    Create 861-FlippingAnImage

---
+++ 861-FlippingAnImage/861-FlippingAnImage.java
@@ -0,0 +1,26 @@
+// Last updated: 7/28/2025, 12:29:45 PM
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image)
+    {  
+       
+        for(int i=0;i<image.length;i++)
+        {
+            int low=0;
+            int high=image[0].length-1;
+            while(low<=high)
+            {
+                if(image[i][low]==image[i][high])
+                {
+                    image[i][low]=1-image[i][low];
+                    image[i][high]=image[i][low];
+                }
+                low++;
+                high--;
+            }
+        }
+        return image;
+        
+     
+        
+    }
+}
