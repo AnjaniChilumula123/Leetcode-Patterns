61590d2df08c3c873ad067637f27d9bf75d1461a
AnjaniChilumula123 <93757236+AnjaniChilumula123@users.noreply.github.com>
2025-07-28 12:31:39 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 7/28/2025, 12:31:40 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor=xor^nums[i];
+        }
+        return xor;
+    }
+}
