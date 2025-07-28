f4aa4f10c29d58b1caafe73842b417c757e8d034
AnjaniChilumula123 <93757236+AnjaniChilumula123@users.noreply.github.com>
2025-07-28 12:31:09 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/28/2025, 12:31:10 PM
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        k = k%n;
+        rev(nums ,0, n-1);
+        rev(nums ,0, k-1);
+        rev(nums,k, n-1);
+    }
+    void rev (vector<int> &nums ,int start , int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start ++ ;
+            end --;
+        }
+    }
+};
