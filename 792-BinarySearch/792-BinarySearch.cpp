9b1d318f263a13942a897644b1d70022972cc8cf
AnjaniChilumula123 <93757236+AnjaniChilumula123@users.noreply.github.com>
2025-07-28 12:29:48 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/28/2025, 12:29:49 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low =  0  , high = nums.size() - 1 ;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
